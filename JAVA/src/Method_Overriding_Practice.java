518f24107afd09d92a432fa07a5f8129ee62f042
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-05-13 17:02:21 +0900

    afternoon update

--- JAVA/src/Method_Overriding_Practice.java
+++ JAVA/src/Method_Overriding_Practice.java
@@ -1,39 +1,33 @@
 public class Method_Overriding_Practice {
     public static void main(String[] args) {
-        Cat[] cats = new Cat[] { new Dusty(), new Toto(), new Galbi()};
+        Cat[] cats = new Cat[]{new Dusty(), new Toto(), new Galbi()};
 
-        for(int i = 0; i<cats.length; i++)
-        {
+        for (int i = 0; i < cats.length; i++) {
             cats[i].sleepmode();
         }
     }
 }
-class Cat
-{
-    void sleepmode()
-    {
+
+class Cat {
+    void sleepmode() {
         System.out.println("zzzz...");
     }
 }
-class Dusty extends Cat
-{
-    void sleepmode()
-    {
+
+class Dusty extends Cat {
+    void sleepmode() {
         System.out.println("안자고 뭐해");
     }
 }
 
-class Toto extends Cat
-{
-    void sleepmode()
-    {
+class Toto extends Cat {
+    void sleepmode() {
         System.out.println("토토 잔다");
     }
 }
-class Galbi extends Cat
-{
-    void sleepmode()
-    {
+
+class Galbi extends Cat {
+    void sleepmode() {
         System.out.println("갈비 잔다");
     }
 }
