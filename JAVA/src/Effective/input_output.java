fb1b6118e668023dce4ce457699019d02a9a7c53
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-05-21 01:18:14 +0900

    study stuffs file uploads

--- JAVA/src/Effective/input_output.java
+++ JAVA/src/Effective/input_output.java
@@ -12,6 +12,7 @@ public class input_output {
             while((data=bfile.read())!= -1){
                 System.out.print((char)data);
             }
+            System.out.println("\n");
             file.close();
         }
             catch(IOException e){
@@ -19,11 +20,11 @@ public class input_output {
 
             }
 
-        File file = new File("codestates11.txt");
-        file.createNewFile();
-        System.out.println(file.getPath());
-        System.out.println(file.getParent());
-        System.out.println(file.getCanonicalPath());
-        System.out.println(file.canWrite());
+        File file = new File("study/IOtesting.txt"); //File 객체 생성 (매개변수 = 파일 이름)
+        file.createNewFile();                                  //createNewFile( )메서드로 실제 파일 생성
+        System.out.println(file.getPath());                    //파일의 경로 String으로 반환
+        System.out.println(file.getParent());        //파일의 상위 디렉토리를 String or 파일로 반환
+        System.out.println(file.getCanonicalPath()); //파일의 정규 경로를 String으로 반환
+        System.out.println(file.canWrite());         //쓸수 있는 타입인지 boolean으로 반환
     }
 }
