3df6a9fe3eda532888af2dc449270bef8d755b1b
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-05-31 23:34:33 +0900

    1 algo practice

--- JAVA/Algorithm/BaekJoon/Array/Array_2577.java
+++ JAVA/Algorithm/BaekJoon/Array/Array_2577.java
@@ -16,15 +16,14 @@ public class Array_2577 {
 
         String str = Integer.toString(mult);
 
-        for(int i = 0; i < 10; ++i) {
+        for(int i = 0; i < 10; ++i) {  // 1~9를 문자 하나씩 비교해야 함
             int count = 0;
 
-            for(int j = 0; j < str.length(); ++j) {
-                if (str.charAt(j) - 48 == i) {
-                    ++count;
+            for(int j = 0; j < str.length(); ++j) { // 문자열 길이만큼 돌면서 한 문자씩 i와 비교
+                if (str.charAt(j) - '0' == i) { // String 숫자 - '0' 하면 숫자 가 된다 (유니코드 이용)
+                    count++;
                 }
             }
-
             System.out.println(count);
         }
 
